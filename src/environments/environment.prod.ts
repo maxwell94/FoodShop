c8e16aa3677e0cfe4135299ce611dfe4f62da411
Max Dedou <maxdedou1994@gmail.com>
2020-03-04 12:25:33 +0100

    initial navbar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDF4tQgbt1d3oqLBaeLSwnZRnIc5WXDQ_U",
+    authDomain: "foodshop-f6d84.firebaseapp.com",
+    databaseURL: "https://foodshop-f6d84.firebaseio.com",
+    projectId: "foodshop-f6d84",
+    storageBucket: "foodshop-f6d84.appspot.com",
+    messagingSenderId: "217582756219",
+    appId: "1:217582756219:web:b62a6bc3154b052963ddde",
+    measurementId: "G-T56M3PDB91"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDF4tQgbt1d3oqLBaeLSwnZRnIc5WXDQ_U",
+    authDomain: "foodshop-f6d84.firebaseapp.com",
+    databaseURL: "https://foodshop-f6d84.firebaseio.com",
+    projectId: "foodshop-f6d84",
+    storageBucket: "foodshop-f6d84.appspot.com",
+    messagingSenderId: "217582756219",
+    appId: "1:217582756219:web:b62a6bc3154b052963ddde",
+    measurementId: "G-T56M3PDB91"
+  }
 };
 
 /*
