399e2f27ebc867ef37c6a63b061a171585676a45
Max Dedou <maxdedou1994@gmail.com>
2020-03-07 01:58:28 +0100

    edit products problems resolved

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from 'src/app/product.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import 'rxjs/add/operator/take';
 
 @Component({
@@ -13,21 +13,34 @@ export class ProductFormComponent implements OnInit {
   
   categories$; 
   product={};
+  id;
 
   constructor(
     private categoryService: CategoryService,
     private productsService: ProductService,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private router: Router) {
      this.categories$ = categoryService.getCategories();
 
-     let id = this.route.snapshot.paramMap.get('id');
-     if (id) this.productsService.get(id).take(1).subscribe(p => this.product = p);
+     this.id = this.route.snapshot.paramMap.get('id');
+     if (this.id) this.productsService.get(this.id).take(1).subscribe(p => this.product = p);
    }
 
-   /** Metodo che salva i dati dal form admin product nel db */
+   /** Metodo che salva o aggiorna i product nel db */
    save(product){
      //console.log(product);
-     this.productsService.create(product);
+     if (this.id) this.productsService.update(this.id,product); // esiste l'id aggiorna
+     else this.productsService.create(product); //altrimenti lo crea 
+
+     this.router.navigate(['/admin/products']);
+   }
+   
+   /**Metodo che cancella  */
+   delete(){
+      if( !confirm('Are you sure you want to delete this product?')) return; 
+      this.productsService.delete(this.id);
+      this.router.navigate(['/admin/products']);
+      
    }
 
   ngOnInit() {
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,16 +8,29 @@ export class ProductService {
 
   constructor(private db:AngularFireDatabase) { }
 
-  /** Crea una nuova entità product da inserire nel db */
+  /** Crea un prodotto da inserire alla lista dei prodotti nel db */
   create(product){
      return this.db.list('/products').push(product);
   }
 
+  /**ottengo tutti i prodotti dal db */
   getAll(){
     return this.db.list('/products');
   }
-
+  
+  /** ottengo un prodotto tra tutti i prodotti */
   get(productId){
     return this.db.object('/products/'+productId);
   }
+
+  /**Aggiorna un prodotto */
+  update(productId, product){
+    return this.db.object('/products/'+productId).update(product);
+  }
+
+  /**Cancella un prodotto */
+  delete(productId){
+    return this.db.object('/products/'+productId).remove();
+  }
+
 }
